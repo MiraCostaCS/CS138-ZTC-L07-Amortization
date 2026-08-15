92727bfdeb0f6e87b7610e20e8dda86b2598b9c7
kevinmlewis <kevinmlewis@gmail.com>
2026-08-15 14:51:30 -0700

    Initialize main.py with program structure
    Added initial structure and docstring for main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+DEVELOPER: <your name>
+COLLABORATORS: <anyone you worked with>
+'''
+"""A one line summary of the program, terminated by a period.
+Leave one blank line. The rest of this docstring should contain an
+overall description of the program.
+"""
+##########################################
+# MAIN PROGRAM:
+##########################################
+#<replace this line with your main program>
